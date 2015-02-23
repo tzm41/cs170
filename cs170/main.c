cb48cf167ee279e67f61f1827926c3caee744384
Zhuoming Tan <gaungzhoutzm@gmail.com>
2015-02-23 16:30:46 -0500

    Initial Commit

---
+++ cs170/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cs170
+//
+//  Created by Colin Tan on 2/23/15.
+//  Copyright (c) 2015 Colin Tan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
